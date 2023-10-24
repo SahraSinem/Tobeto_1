8dbfab0a807ace29d82509e4ef45ea4ef60846a2
SahraSinem <s.sinem_can@hotmail.com>
2023-10-24 21:29:29 +0300

    Bolum 1-14:

---
+++ TypesAndVariables/Program.cs
@@ -0,0 +1,21 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //  Console.WriteLine("Hello, World!");
+        double number5 = 10.4;
+        decimal number6 = 10;
+        int number1 = 1;
+        long number2 = 9223372036854775807;
+        short number3 = 32767;
+        byte number4 = 255;
+        char character = 'A';
+        bool condition = false;
+        var number7 = 10;
+        Console.WriteLine("Number1 is {0}", number1);
+        Console.WriteLine("Number2 is {0}", number2);
+        Console.WriteLine("Number3 is {0}", number3);
+        Console.WriteLine("Number4 is {0}", number4);
+        Console.WriteLine("Number5 is {0}", number5);
+        Console.WriteLine("Character is : {0}", (int)character);
+    }
