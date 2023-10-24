1793dd8741a98547ccd3fda301a7e3f19851eee5
SahraSinem <s.sinem_can@hotmail.com>
2023-10-25 02:33:35 +0300

    Video: 26-27

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -6,12 +6,15 @@
         {
             //Add();
             //var result = Add2(20, 30);
-            int number1 = 20;
-            int number2 = 100;
-            var result2 = Add3(number1, number2);
-            Console.WriteLine(result2);
-            Console.WriteLine(number1);
-        }
+            //int number1 = 20;
+            //int number2 = 100;
+            //var result2 = Add3(number1, number2);
+            //Console.WriteLine(result2);
+            //Console.WriteLine(number1);
+            Console.WriteLine(Multiply(2, 4));
+            Console.WriteLine(Multiply(2, 4, 5));
+            Console.WriteLine(Add4(1,2,3,4,5,6));
+        } }
 
         static void Add()
         {
@@ -23,10 +26,23 @@
             var result = number1 + number2;
             return result;
         }
-        static int Add3(int number1, int number2) 
+        static int Add3(out int number1, int number2)
         {
             number1 = 30;
-        return number1 + number2;
+            return number1 + number2;
         }
+        static int Multiply(int number1, int number2)
+        {
+            return number1 * number2;
+        }
+        static int Multiply(int number1, int number2, int number3)
+        {
+            return number1 * number2 * number3;
+        }
+        static int Add4(int number,params int[] numbers)
+    {
+        return numbers.Sum();
     }
-}
+
+    }
+
