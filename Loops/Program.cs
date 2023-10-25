a0cf26ae254f414ad127e9aee5a8fb8270007f2f
SahraSinem <s.sinem_can@hotmail.com>
2023-10-25 04:38:05 +0300

    Video:34-35

--- Loops/Program.cs
+++ Loops/Program.cs
@@ -5,14 +5,37 @@
         //  ForLoop();
         //  WhileLoop();
         // DoWhileLoop();
+        // ForEachLoop();
+        if (IsPrimeNumb(6))
+        {
+            Console.WriteLine("This is a prime number");
+        }
+        else
+        {
+            Console.WriteLine("This is not a prime number");
+        }
+
+    }
+    private static bool IsPrimeNumber(int number)
+    {
+        bool result = true;
+        for (int i = 2; i < number - 1; i++)
+        {
+            if (number % i == 0)
+            {
+                result = false;
+                i = number;
+            }
+        }
+    }
+    private static void ForEachLoop()
+    {
         string[] students = new string[3] { "Engin", "Derin", "Salih" };
         foreach (var student in students)
         {
             Console.WriteLine(student);
         }
-
     }
-
     private static void DoWhileLoop()
     {
         int number = 10;
